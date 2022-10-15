48e1575884449e1dbfd302e12085c9a876382560
Juan-David-Orozco <ing.ele2018@gmail.com>
2022-10-15 10:30:57 -0500

    initial route added

--- index.js
+++ index.js
@@ -2,5 +2,7 @@ const express = require("express")
 
 const app = express()
 
+app.get('/', (req, res) => res.send("Hello world"))
+
 app.listen(process.env.PORT || 4000)
 console.log("Server on port", process.env.PORT || 4000)
