8a55d508fe0f15a5489211c0fd6c3b1750e75b42
Juan-David-Orozco <ing.ele2018@gmail.com>
2022-10-15 12:34:29 -0500

    template engine added

--- index.js
+++ index.js
@@ -1,8 +1,25 @@
 const express = require("express")
+const ejs = require("ejs")
+const path = require("path")
 
 const app = express()
 
-app.get('/', (req, res) => res.send("Hello world"))
+/* Settings */
+app.set("view engine", "ejs")
+app.set('views', path.join(__dirname, 'views'))
 
+/* Routes */
+//app.get('/', (req, res) => res.send("Hello world"))
+
+app.get('/api/users', (req, res) => res.json([{name: 'ryan'}, {name: 'joe'}]))
+
+app.get('/profile', (req, res) => {
+  res.render('profile', {name: 'juan', age: 29})
+})
+
+/* Static Files */
+app.use(express.static('public'))
+
+/* Run Server */
 app.listen(process.env.PORT || 4000)
 console.log("Server on port", process.env.PORT || 4000)
